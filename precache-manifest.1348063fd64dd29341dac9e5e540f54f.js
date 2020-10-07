147c54d4032c028c3cb36c31325fdd575761b450
jutelles <julianatelles.contato@gmail.com>
2020-10-07 03:02:11 -0300

    Updates

---
+++ precache-manifest.1348063fd64dd29341dac9e5e540f54f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96440d9cffb622eb9a26cf50e7b06360",
+    "url": "/compound-interest-calculator/index.html"
+  },
+  {
+    "revision": "2dea4d29229960c1dbd3",
+    "url": "/compound-interest-calculator/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "c7f3d1997c4213f2ce51",
+    "url": "/compound-interest-calculator/static/css/main.464df1a8.chunk.css"
+  },
+  {
+    "revision": "2dea4d29229960c1dbd3",
+    "url": "/compound-interest-calculator/static/js/2.95c54e45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/compound-interest-calculator/static/js/2.95c54e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f3d1997c4213f2ce51",
+    "url": "/compound-interest-calculator/static/js/main.e7597a91.chunk.js"
+  },
+  {
+    "revision": "03835ea635179da4c426",
+    "url": "/compound-interest-calculator/static/js/runtime-main.50773815.js"
+  }
+]);
--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Form from './components/form/form.js';
-
-export default function App() {
-  return <Form />;
-}
